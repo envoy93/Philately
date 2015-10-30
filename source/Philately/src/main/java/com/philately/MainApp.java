2802a5d0c08dfbcfe240ff3b450f7cd69f82b2a9
envoy93 <envoy93@gmail.com>
2015-10-30 17:53:58 +0400

    add H2 DB + simple test

--- source/Philately/src/main/java/com/philately/MainApp.java
+++ source/Philately/src/main/java/com/philately/MainApp.java
@@ -1,10 +1,13 @@
 package com.philately;
 
+import com.philately.model.HibernateUtil;
+import com.philately.model.Stock;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.hibernate.classic.Session;
 
 /**
  * Created by kirill on 22.10.2015.
@@ -20,8 +23,19 @@ public class MainApp extends Application {
         String fxmlFile = "/fxml/hello.fxml";
         FXMLLoader loader = new FXMLLoader();
         Parent root = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
-        stage.setTitle("JavaFX and Maven");
+        stage.setTitle("Maven + JavaFX + Hibernate + H2");
         stage.setScene(new Scene(root));
         stage.show();
+
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+        session.beginTransaction();
+        Stock stock = new Stock();
+
+        stock.setStockCode("4715");
+        stock.setStockName("GENM");
+
+        session.save(stock);
+        session.getTransaction().commit();
     }
 }
